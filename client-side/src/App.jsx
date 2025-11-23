f5e0641061cf056640f988991570bd838b671a90
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-23 13:38:23 +0530

    routing setup

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
