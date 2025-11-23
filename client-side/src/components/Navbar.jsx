8d7c71faa43db80aefdbd82f68341ca059826784
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-23 15:06:40 +0530

    navbar responsive

--- client-side/src/components/Navbar.jsx
+++ client-side/src/components/Navbar.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
-  return (
-    <div className='flex justify-between items-center lg:mx-10 mx-8 py-2'>
-        <Link to={'/'}>
-            <img className='h-12' src="/Logo.png" alt="logo" />
-        </Link>
-        <div className='w-[30em]'>
-            <ul className='flex justify-evenly'>
-                <Link to={'/'}>
-                    <li className=' text-xl cursor-pointer hover:text-blue-600'>Home</li>
-                </Link>
-                <Link to={'/categories'}>
-                    <li className=' text-xl cursor-pointer hover:text-blue-600'>Categories</li>
-                </Link>
-                <li className=' text-xl cursor-pointer hover:text-blue-600'>Dashboard</li>
-                <li className=' text-xl cursor-pointer hover:text-blue-600'>Services</li>
-            </ul>
+    return (
+        <div className='flex justify-between items-center lg:px-10 px-5 py-2 shadow-sm'>
+            <Link to={'/'}>
+                <img className='lg:h-12 h-9' src="/Logo.png" alt="logo" />
+            </Link>
+            <div id='menubar' className='bg-gray-200 px-2 py-1 rounded'>
+                <MenuIcon />
+            </div>
+            <div id='nav-link' className='w-180 flex justify-between'>
+                <ul className='flex gap-10'>
+                    <Link to={'/'}>
+                        <li className=' text-xl cursor-pointer hover:text-blue-600'>Home</li>
+                    </Link>
+                    <Link to={'/categories'}>
+                        <li className=' text-xl cursor-pointer hover:text-blue-600'>Categories</li>
+                    </Link>
+                    <li className=' text-xl cursor-pointer hover:text-blue-600'>Dashboard</li>
+                    <li className=' text-xl cursor-pointer hover:text-blue-600'>Services</li>
+                </ul>
+                <div className='flex gap-5'>
+                    <Button variant="contained">List Your Business</Button>
+                    <img className='lg:w-8 w-7 cursor-pointer hover:opacity-70' src="../src/assets/bell-icon.svg" alt="notification" />
+                </div>
+            </div>
         </div>
-        <div className='flex gap-5'>
-            <Button variant="contained">List Your Business</Button>
-            <img className='w-8 cursor-pointer hover:opacity-70' src="../src/assets/bell-icon.svg" alt="notification" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar;
