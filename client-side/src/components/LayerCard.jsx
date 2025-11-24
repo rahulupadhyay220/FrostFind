d1834ceea16be096ec258acd73c523da92d0bf1f
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-24 18:48:02 +0530

    layer section

---
+++ client-side/src/components/LayerCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const LayerCard = ({image, title, description, tagLevel1, tagLevel2}) => {
+  return (
+    <div className='lg:w-100 w-full border p-10 rounded-xl border-gray-200 mb-5 hover:shadow-lg hover:border-blue-200 transition-all duration-100'>
+        <img className='bg-blue-200 p-2 w-10 rounded-sm' src={image} alt="image-icon" />
+        <div>
+            <h3 className='text-xl my-4'>{title}</h3>
+            <p className='text-gray-500'>{description}</p>
+        </div>
+        <div className='flex gap-6 mt-4'>
+            <p className='bg-gray-200 text-sm text-gray-800 px-3 py-1 rounded-full'>{tagLevel1}</p>
+            <p className='bg-gray-200 text-sm text-gray-800 px-3 py-1 rounded-full'>{tagLevel2}</p>
+        </div>
+    </div>
+  )
+}
+
+export default LayerCard;
--- client-side/src/components/TrustLayer.jsx
+++ client-side/src/components/TrustLayer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import LayerCard from './LayerCard'
 
 const TrustLayer = () => {
   return (
-    <div>TrustLayer</div>
+    <div className='lg:px-10 px-5'>
+      <div className='mt-30 mb-20'>
+        <h1 className='text-center lg:text-4xl text-3xl font-semibold'>Why Trust Matters More Than Stars</h1>
+        <p className='text-gray-500 text-center my-3 lg:text-xl md:text-xl text-sm'>Every review is unique. Every flagged review deserves an explanation.</p>
+      </div>
+      <div className='flex justify-evenly lg:flex-row flex-col'>
+        <LayerCard title={"Explainability First"} image={"../src/assets/security.png"} description={"We don't just filter—we explain. See exactly why we flagged that review."} tagLevel1={"Transparency"} tagLevel2={"Clarity"} />
+        <LayerCard title={"Real-Time Detection"} image={"../src/assets/thunder.png"} description={"Advanced NLP and pattern analysis catch manipulation instantly, protecting your purchases."} tagLevel1={"Fast"} tagLevel2={"Accurate"} />
+        <LayerCard title={"Multi-Layered Verification"} image={"../src/assets/users.svg"} description={"Consumer app, business dashboard, and marketplace widgets—trust across all platforms."} tagLevel1={"Modular"} tagLevel2={"Scalable"} />
+      </div>
+    </div>
   )
 }
 
