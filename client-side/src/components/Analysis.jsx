74d335c3c7f138570849a395d0867918bb66dfcf
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-24 18:02:58 +0530

    Analysis section

--- client-side/src/components/Analysis.jsx
+++ client-side/src/components/Analysis.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import AnalysisCard from './AnalysisCard';
 
 const Analysis = () => {
   return (
-    <div>Analysis</div>
+    <div className='lg:mx-15 mx-5 lg:px-10 px-5 mb-8 flex lg:flex-row flex-col bg-blue-100 rounded-2xl'>
+        <AnalysisCard heading={"Consumer App"} image={"../src/assets/circle-check.svg"} title={"Smart Review Analysis"} description={"Real-time authenticity detection with clear explanations"} />
+        <AnalysisCard heading={"Business Dashboard"} image={"../src/assets/chart.svg"} title={"Trust Analytics"} description={"Monitor product credibility and reviewer patterns"} />
+    </div>
   )
 }
 
---
+++ client-side/src/components/AnalysisCard.jsx
@@ -0,0 +1,36 @@
+import Slider from '@mui/material/Slider';
+import React from 'react';
+
+const AnalysisCard = ({image, title, heading, description}) => {
+  return (
+    <div className='lg:p-15 md:p-10 py-10 px-5 w-full'>
+        <h3 className='text-xl font-medium mb-4'>{heading}</h3>
+        <div className='flex items-center gap-5'>
+            {heading === "Consumer App" ? (
+                <img className='w-13 h-13 lg:w-10 md:w-12 md:h-12 lg:h-10 rounded bg-[#b7e4c7] p-1.5' src={image} alt="icon" />
+            ) : 
+                <img className='w-13 h-13 lg:w-10 md:w-12 md:h-12 lg:h-10 rounded bg-blue-200 p-1.5' src={image} alt="icon" />
+            }
+            <div>
+                <h3 className='text-[18px] font-medium'>{title}</h3>
+                <p className='text-sm text-gray-500'>{description}</p>
+            </div>
+        </div>
+        {heading === "Consumer App" ? (
+            <div className='text-sm text-green-800 bg-[#b7e4c7] w-35 rounded-xl border p-2 mt-3 flex gap-2'>
+                <img className='w-5' src="../src/assets/check.svg" alt="check icon" />
+                <p className='font-medium'>Likely Genuine</p>
+            </div>
+        ) : 
+        <>
+            <div className='bg-gray-300 lg:w-100 md:w-100  h-1.5 rounded-full mt-5'>
+                <div className='lg:w-84 md:w-84 w-50 h-1.5 rounded-full bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500'></div>
+            </div>
+            <p className='mt-5 text-sm text-gray-500'>Trust Score : 84%</p>    
+        </>
+        }
+    </div>
+  )
+}
+
+export default AnalysisCard;
