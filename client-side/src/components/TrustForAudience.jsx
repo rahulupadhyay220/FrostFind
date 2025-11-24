bb83ab15c61ae7c44c51e5cf250738d52875dca0
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-24 23:10:08 +0530

    audience section update

--- client-side/src/components/TrustForAudience.jsx
+++ client-side/src/components/TrustForAudience.jsx
@@ -33,7 +33,7 @@ const TrustForAudience = () => {
       </div>
       <div className='bg-blue-100 rounded-2xl lg:p-10 p-5 lg:w-160 w-full flex flex-col gap-5'>
         <Sample title={"Product: Samsung Galaxy S24"} description={"Best phone ever! Fast, reliable, amazing camera..."} level={"verify"} />
-        <Sample title={"Product: Samsung Galaxy S24"} description={"Best phone ever! Fast, reliable, amazing camera..."} level={"flag"} />
+        <Sample title={"Product: Samsung Galaxy S24"} description={"Excellent product excellent product excellent product..."} level={"flag"} />
       </div>
     </div>
   )
