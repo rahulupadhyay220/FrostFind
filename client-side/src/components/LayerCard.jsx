e1baed8e31a49e6bdb9158390f991dfdb51e0ac7
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-12-02 22:27:04 +0530

    card fix

--- client-side/src/components/LayerCard.jsx
+++ client-side/src/components/LayerCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LayerCard = ({image, title, description, tagLevel1, tagLevel2}) => {
   return (
-    <div className='lg:w-100 w-full border p-10 rounded-xl border-gray-200 mb-5 hover:shadow-lg hover:border-blue-200 transition-all duration-100'>
+    <div className='lg:w-100 lg:m-3 w-full border p-10 rounded-xl border-gray-200 mb-5 hover:shadow-lg hover:border-blue-200 transition-all duration-100'>
         <img className='bg-blue-200 p-2 w-10 rounded-sm' src={image} alt="image-icon" />
         <div>
             <h3 className='text-xl my-4'>{title}</h3>
